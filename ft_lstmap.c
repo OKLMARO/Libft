225a71d4caa42587ed41d1cf1e98226aaa17b01f
Omar Amairi <oamairi@bess-f4r2s2.clusters.42paris.fr>
2025-04-24 16:33:37 +0200

    finish 1.0.1v

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 16:06:09 by oamairi           #+#    #+#             */
-/*   Updated: 2025/04/24 16:32:02 by oamairi          ###   ########.fr       */
+/*   Updated: 2025/04/24 16:33:33 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*lst_new;
 
-	lstNew = ft_lstnew();
+	lst_new = ft_lstnew();
 	while (lst->next)
 	{
-		lstNew = f(lst);
+		lst_new = f(lst);
 		lst = lst->next;
-		lstNew->next = ft_lstnew();
+		lst_new->next = ft_lstnew();
 	}
-	lstNew = f(lst);
-	return (lstNew);
+	lst_new = f(lst);
+	return (lst_new);
 }
