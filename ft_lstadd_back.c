b64aa1a6aa045c88d99392bff0bb164b0e1f72bd
Omar Amairi <oamairi@bess-f4r2s1.clusters.42paris.fr>
2025-04-24 13:29:03 +0200

    avancement non obligatoire lstclear

---
+++ ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 13:10:20 by oamairi           #+#    #+#             */
+/*   Updated: 2025/04/24 13:15:13 by oamairi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	temp = *lst;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: oamairi <oamairi@student42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 11:51:27 by oamairi           #+#    #+#             */
-/*   Updated: 2025/04/10 11:51:27 by oamairi          ###   ########.fr       */
+/*   Updated: 2025/04/24 12:19:41 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,11 @@ int	ft_isalpha(int chr);
 int	ft_isdigit(int chr);
 int	ft_isalnum(int chr);
 int	ft_strlen(char *str);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 #endif
